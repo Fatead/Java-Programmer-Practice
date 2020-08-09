c3cb74717ecf5fbf756753478eafa0ccd8c719d1
Fatead <893051689@qq.com>
2020-08-09 14:20:26 +0800

    threadPool

---
+++ src/thread/FixedThreadPool.java
@@ -0,0 +1,50 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class FixedThreadPool {
+
+
+    /**
+     * 实现Callable接口实现有返回值的多线程
+     * 利用对象Future的get方法获取到Callable任务返回的Object
+     */
+    private static class MyCallable implements Callable{
+
+        String str;
+
+        MyCallable(String str){
+            this.str = str;
+        }
+
+        @Override
+        public Object call() throws Exception {
+            return this.str;
+        }
+    }
+
+    private static int taskSize = 10;
+
+    private void execute() throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(taskSize);
+        List<Future> list = new ArrayList<>();
+        for(int i = 0;i<taskSize;i++){
+            Callable callable = new MyCallable(i + " ");
+            Future f = executorService.submit(callable);
+            list.add(f);
+        }
+        executorService.shutdown();
+        for(Future f:list){
+            System.out.println("res:" + f.get().toString());
+        }
+    }
+
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        FixedThreadPool fixedThreadPool = new FixedThreadPool();
+        fixedThreadPool.execute();
+    }
+
+}
---
+++ src/thread/Scheduled.java
@@ -0,0 +1,41 @@
+package thread;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Scheduled {
+
+
+    private int taskNum = 3;
+
+    private class MyTask implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("延迟三秒");
+        }
+
+    }
+
+    private void execute(){
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(3);
+        List<Runnable> runnableList = new ArrayList<>();
+        for(int i =0;i<taskNum;i++){
+            Runnable task = new MyTask();
+            runnableList.add(task);
+        }
+        for(Runnable runnable:runnableList){
+            scheduledExecutorService.schedule(runnable,3, TimeUnit.SECONDS);
+        }
+        scheduledExecutorService.shutdown();
+    }
+
+    public static void main(String[] args) {
+        Scheduled scheduled = new Scheduled();
+        scheduled.execute();
+    }
+
+}
